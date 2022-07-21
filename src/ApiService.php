300744eaf9d920e7854558d9e64b05574dff9105
Rander <randerccf@gmail.com>
2022-07-21 19:15:12 -0300

    feat: Initial commit

---
+++ src/ApiService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Services;
+
+use Illuminate\Support\Facades\Http;
+
+abstract class ApiService {
+    protected string $endpoint;
+
+    public function request($method, $path, $data = []) {
+
+        try {
+            $response = Http::acceptJson()
+                ->retry(2, 100)
+                ->withToken(request()->cookie('jwt') ?? request()->bearerToken() ?? null)
+                ->$method("{$this->endpoint}/{$path}", $data);
+
+//            abort_if($response->failed(), $response->status(), optional($response->json())['message']);
+
+            return $response->json();
+        } catch(\Throwable $exception) {
+            abort($exception->getCode(), $exception->getMessage());
+        }
+    }
+
+    public function post($path, $data) {
+        return $this->request('post', $path, $data);
+    }
+
+    public function get($path) {
+        return $this->request('get', $path);
+    }
+
+    public function put($path, $data) {
+        return $this->request('put', $path, $data);
+    }
+
+    public function delete($path) {
+        return $this->request('delete', $path);
+    }
+
+    public function patch($path, $data) {
+        return $this->request('patch', $path, $data);
+    }
+}
---
+++ src/UserService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Services;
+
+class UserService extends ApiService
+{
+    public function __construct()
+    {
+        // $this->endpoint = 'http://users-microservice:8000/api';
+        $this->endpoint = config('microservices.users-microservice.url') . '/api';
+    }
+
+    public function register(array $data)
+    {
+        return $this->post('register', $data);
+    }
+
+    public function login(array $data)
+    {
+        return $this->post('login', $data);
+    }
+
+    public function getLoggedUser()
+    {
+        return $this->get('logged-user');
+    }
+
+    public function logout()
+    {
+        return $this->post('logout', []);
+    }
+
+    public function updateLoggedUserInfo(array $data)
+    {
+        return $this->put('logged-user/info', $data);
+    }
+
+    public function updateLoggedUserPassword(array $data)
+    {
+        return $this->put('logged-user/password', $data);
+    }
+
+    public function getUsers()
+    {
+        return $this->get('users');
+    }
+
+    public function getUser(int $userId)
+    {
+        return $this->get("users/$userId");
+    }
+
+    public function scopeCan(string $permission)
+    {
+        return $this->get("scope/$permission");
+    }
+}
